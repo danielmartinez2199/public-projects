79dc367e74e64c9730ab6485c0727e79015195e7
danielmartinez2199 <124226209+danielmartinez2199@users.noreply.github.com>
2023-05-14 10:56:35 -0700

    Updated with function descriptions.

--- Stock History Analysis_Dashboard_5-8-2023_Working.py
+++ Stock History Analysis_Dashboard_5-8-2023_Working.py
@@ -1,35 +1,49 @@
-import yfinance as yf
+from datetime import datetime
 import pandas as pd
 import numpy as np
+import yfinance as yf
 from bokeh.io import show
-from bokeh.models import ColumnDataSource, DatetimeTickFormatter, Div, LinearColorMapper, ColorBar
+from bokeh.models import DatetimeTickFormatter, Div, LinearColorMapper, ColorBar
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
-from datetime import datetime, timedelta
 from bokeh.models.formatters import NumeralTickFormatter
 
 def user_input():
-    # define the stock ticker symbol
-    # user inputs
+    """
+    Prompts the user for input and returns a tuple of the ticker symbol choice, 
+    period start and end dates, initial number of shares owned (if applicable), 
+    and initial purchase date of shares (if applicable).
+    """
     choice = str(input('Please enter the ticker symbol here: '))
     start_date = str(input('Please enter start date (YYYY-MM-DD): '))
     end_date = str(input('Please enter end date (YYYY-MM-DD): '))
-    stock_input = float(input('Please enter number of shares owned, otherwise enter 0: '))
+    shares_input = float(input('Please enter number of shares owned, otherwise enter 0: '))
     purchase_date = str(input('Please enter first purchase date (YYYY-MM-DD), otherwise enter 0: '))    
-    return choice, start_date, end_date, stock_input, purchase_date
+
+    return choice, start_date, end_date, shares_input, purchase_date
 
 def get_purchase_info():
+    """
+    Prompts the user to enter purchase dates and owned number of shares, and 
+    returns a dictionary where the keys are purchase dates (YYYY-MM-DD) and 
+    the values are the corresponding number of shares.
+    """
     purchase_info = {}
     while True:
         purchase_date = input('Enter purchase date (YYYY-MM-DD), or press enter to finish: ')
         if not purchase_date:
             break
         shares_owned = float(input('Enter number of shares owned: '))
-        purchase_info[purchase_date] = shares_owned
+        purchase_info[purchase_date] = shares_owned\
+        
     return purchase_info
 
 def ticker(choice, start_date, end_date):
-    # retrieve the stock data
+    """
+    Function inputs: ticker choice, start and end dates of stock history.
+    Prints the user selected ticker symbol and company name. 
+    Returns the stock data from the user inputted choice.
+    """
     print(f"You selected {choice}")
     ticker = yf.Ticker(choice)
     company_name = ticker.info['longName']
@@ -37,105 +51,107 @@ def ticker(choice, start_date, end_date):
     print(f'You selected {start_date} to {end_date} as the period')
     print('The dashboard will now open on your browser as an HTML file')
     stock_data = ticker.history(start=start_date, end=end_date)
+
     return stock_data, company_name
 
 def dataframe(stock_data):
-    # create a dataframe
+    """
+    Function input: stock data from ticker function.
+    Creates a dataframe. Removes the time from the date index column.
+    Resets the index and uses the date as the index column.
+    Sorts the index date column and returns the dataframe.
+    """
     df = pd.DataFrame(stock_data, columns=['Open','High','Low','Close','Volume'])
-
-    # remove the time from the date index column
     df.index = df.index.date
-
-    # reset index and use date as index column
     df = df.reset_index()
     df = df.rename(columns={'index':'Date'})
     df['Date'] = pd.to_datetime(df['Date'])
-
-    # sort the dataframe by date
     df = df.sort_values(by=['Date'])
 
     return df
 
 def create_scatter_plot(df,company_name):
-    # creates scatter plot
+    """
+    Function input: dataframe and company name from dataframe function.
+    Creates the scatter plot and formats the chart with the date as x-axis and the open price as the y-axis with a navy blue color.
+    The chart outputs the date in mmm yyyy format and finally returns the chart.
+    """
     title = f'Open Prices Over Time for {company_name}'
     p = figure(title=title, x_axis_label='', y_axis_label='Open Price')
-
-    # formatting properties
     p.scatter(df['Date'], df['Open'], color='navy')
     p.xaxis[0].formatter = DatetimeTickFormatter(months='%b %Y')
     return p
 
 def create_candlestick_chart(df,company_name):
-    # creates candlestick chart
+    """
+    Function input: dataframe and company name from dataframe function.
+    Creates the candlestick chart and formats the chart with the date as the x-axis and the open/close prices as the y-axis.
+    The closing price is the top of each "candlestick", while the open price is the bottom of each "candlestick".
+    The chart outputs the date in mmm yyyy format and finally returns the chart.
+    """
     title = f'Candlestick Chart for {company_name}'
     inc = df.Close > df.Open
     dec = df.Open > df.Close
     w = 12*60*60*1000 # half day in ms
     p = figure(x_axis_type="datetime", title = title,x_axis_label='')
-
-    # formatting properties
     p.xaxis.major_label_orientation = np.pi/4
     p.grid.grid_line_alpha=0.3
     p.segment(df.Date, df.High, df.Date, df.Low, color="black")
     p.vbar(df.Date[inc], w, df.Open[inc], df.Close[inc], fill_color="#D5E1DD", line_color="black")
     p.vbar(df.Date[dec], w, df.Open[dec], df.Close[dec], fill_color="#F2583E", line_color="black")
-
-    # formats date on x-axis as mmm yyyy
     p.xaxis[0].formatter = DatetimeTickFormatter(months='%b %Y')
     return p
 
 def create_bar_chart(df,company_name):
-    # creates bar chart
+    """
+    Function input: dataframe and company name from dataframe function.
+    Creates a bar chart for the volume of trading activity. X-axis = dates and the y-axis = volume.
+    Adds a legend and formats the volume to units of millions in shares. 
+    """
     title = f'Volume of Trading Activity for {company_name}'
-
-    # creates the figure
     p = figure(title=title, x_axis_label='', y_axis_label='Volume')
-    # Add the bars to the chart
     p.vbar(x=df['Date'], top=df['Volume'], width=0.5, color='green', 
        fill_alpha=0.5, legend_label='Volume')
-    
-    # Set the chart properties
     p.legend.title = 'Volume'
     p.legend.location = "top_right"
     p.legend.label_text_font_size = "10pt"
     p.xaxis.major_label_orientation = 1.2
     p.xaxis[0].formatter = DatetimeTickFormatter(months='%b %Y')
-
-    # set y-axis formatter to millions
     p.yaxis.formatter = NumeralTickFormatter(format='0.00a')
     return p
 
 def create_heatmap(df, company_name):
-    # Create a new DataFrame for heatmap
+    """
+    Function input: dataframe and company name from dataframe function.
+    Creates a new dataframe for the heatmap. Returns the corresponding day of the dates and the close price between the start and end date.
+    Creates a color mapper based on close price and heatmap figure with rectangular glyphs and color bar (on the right)
+    """
     heatmap_df = pd.DataFrame(columns=['Date', 'Day', 'Close'])
 
-    # Iterate over each date in the DataFrame
     for index, row in df.iterrows():
         date = row['Date']
         weekday = date.strftime('%A')
         close_price = row['Close']
         heatmap_df = pd.concat([heatmap_df, pd.DataFrame({'Date': [date], 'Day': [weekday], 'Close': [close_price]})])
 
-    # Define color mapper based on closing price
     color_mapper = LinearColorMapper(palette='RdYlGn11', low=heatmap_df['Close'].min(), high=heatmap_df['Close'].max())
-
-    # Create heatmap figure
     title = f'Stock History Heatmap for {company_name}'
     heatmap = figure(title=title, x_axis_label='', y_axis_label='Day of Week',
                      x_axis_type='datetime', y_range=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
     heatmap.xaxis[0].formatter = DatetimeTickFormatter(months='%b %Y')
     
-    # Add heatmap rectangle glyphs
     heatmap.rect(x='Date', y='Day', width=86400000, height=1, source=heatmap_df,
                  fill_color={'field': 'Close', 'transform': color_mapper}, line_color=None)
 
-    # Add color bar to the right of the heatmap
     color_bar = ColorBar(color_mapper=color_mapper, label_standoff=12, location=(0, 0))
     heatmap.add_layout(color_bar, 'right')
     return heatmap
 
-def share_price_at_dates(df, stock_input, purchase_date, purchase_info):
+def share_price_at_dates(df, shares_input, purchase_date, purchase_info):
+    """
+    Function input: dataframe from dataframe function, number of share(s) owned and purchase date(s) from get_purchase_info function
+    Calculates the accumulation of shares, total share cost, average cost basis, current share price, current total stock value, and gain or loss value.
+    """
     total_shares = 0
     total_cost = 0
     average_cost_basis = 0
@@ -150,7 +166,7 @@ def share_price_at_dates(df, stock_input, purchase_date, purchase_info):
             total_cost += purchase_cost
             total_shares += shares_owned
 
-        total_shares += stock_input
+        total_shares += shares_input
 
         if total_shares == 0:
             print('No shares owned')
@@ -164,6 +180,10 @@ def share_price_at_dates(df, stock_input, purchase_date, purchase_info):
     return total_cost, total_shares, average_cost_basis, current_value, gain_or_loss
 
 def purchase_details(purchase_date, end_date, total_cost, total_shares, average_cost_basis, current_value, gain_or_loss):
+    """
+    Function input: dataframe from dataframe function, number of share(s) owned and purchase date(s) from get_purchase_info function
+    Calculates the accumulation of shares, total share cost, average cost basis, current share price, current total stock value, and gain or loss value.
+    """
     output = []
     output.append(f"Total Cost: ${total_cost:,.2f}")
     output.append(f"Total Shares: {total_shares:,.2f}")
@@ -176,6 +196,10 @@ def purchase_details(purchase_date, end_date, total_cost, total_shares, average_
     return output
 
 def create_dashboard_text(purchase_date, end_date, total_cost, total_shares, average_cost_basis, current_value, gain_or_loss):
+    """
+    Function input: end date of stock history from user input function, number of share(s) owned and purchase date(s) from get_purchase_info function, share costs and values, and gain or loss from get_purchase_details function
+    Calculates the accumulation of shares, total share cost, average cost basis, current share price, current total stock value, and gain or loss value.
+    """
     output = purchase_details(purchase_date, end_date, total_cost, total_shares, average_cost_basis, current_value, gain_or_loss)
     text = "<br>".join(output)
     if gain_or_loss > 0:
@@ -186,6 +210,12 @@ def create_dashboard_text(purchase_date, end_date, total_cost, total_shares, ave
     return div
 
 def show_dashboard(p1, p2, p3, p4, div, company_name, choice, start_date, end_date):
+    """
+    Function inputs: charts from chart functions above, dashboard text, ticker symbol and company name, start and end dates of stock history.
+    Adds a title and disclaimer to the dashboard layout.
+    Organizes column layout in three columns.    
+    Returns dashboard layout
+    """
     # create a new column layout
     col = column()
 
@@ -214,7 +244,7 @@ def show_dashboard(p1, p2, p3, p4, div, company_name, choice, start_date, end_da
     # create a new column layout for div
     div_column = column(div)
 
-    # create a new row layout with p1_p3_column and p2_p4_column as columns
+    # create a new row layout with p1_p3_column, p2_p4_column, and div_column as columns
     heatmap_bubble_row = row(p1_p3_column, p2_p4_column,div_column)
 
     # add heatmap_bubble_row to a new row layout
@@ -227,7 +257,7 @@ def show_dashboard(p1, p2, p3, p4, div, company_name, choice, start_date, end_da
     show(col)
 
 def main():
-    choice, start_date, end_date, stock_input,purchase_date = user_input()
+    choice, start_date, end_date, shares_input,purchase_date = user_input()
     stock_data, company_name = ticker(choice, start_date, end_date)
     df = dataframe(stock_data)
     p1 = create_scatter_plot(df,company_name)
@@ -235,7 +265,7 @@ def main():
     p3 = create_bar_chart(df,company_name)
     p4 = create_heatmap(df,company_name)
     purchase_info = get_purchase_info()
-    total_cost, total_shares, average_cost_basis, current_value, gain_or_loss = share_price_at_dates(df, stock_input, purchase_date, purchase_info)
+    total_cost, total_shares, average_cost_basis, current_value, gain_or_loss = share_price_at_dates(df, shares_input, purchase_date, purchase_info)
     purchase_details(purchase_date, end_date, total_cost, total_shares, average_cost_basis, current_value, gain_or_loss)
     div = create_dashboard_text(purchase_date, end_date, total_cost, total_shares, average_cost_basis, current_value, gain_or_loss)
     show_dashboard(p1, p2, p3, p4, div, company_name, choice, start_date, end_date)
